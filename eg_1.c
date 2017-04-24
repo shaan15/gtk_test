98f5307fefc19c7959a19d648063f1fab8a4de00
Shaan Chopra <shaan15090@iiitd.ac.in>
2017-04-24 17:15:14 +0530

    Update eg_1.c

--- eg_1.c
+++ eg_1.c
@@ -11,6 +11,7 @@ create_row (const gchar *text)
   row = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
   label = gtk_label_new (text);
   gtk_container_add (GTK_CONTAINER (row), label);
+  gtk_widget_show_all(row);
   return row;
 }
 
@@ -19,21 +20,13 @@ next_page (GtkApplication *app,
            GtkBox *box)
 {
   time_row=g_timer_new();
-  GtkWidget *list,*row,*s,*window;
+  GtkWidget *list,*row;
   gchar *text;
-  int status;
   gtk_init (NULL, NULL);
-
-  //window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  //gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
 
   list = gtk_list_box_new ();
   gtk_list_box_set_selection_mode (GTK_LIST_BOX (list), GTK_SELECTION_NONE);
   gtk_box_pack_start (box, list, TRUE, TRUE, 300);
-
-  //s = gtk_scrolled_window_new (NULL, NULL);
-  //gtk_container_add (GTK_CONTAINER (app), s);
-  //gtk_container_add (GTK_CONTAINER (app), box);
 
   gint i;
   g_timer_reset (time_row);
@@ -42,14 +35,14 @@ next_page (GtkApplication *app,
   {
     text=g_strdup_printf("Row %d",i);
     row=create_row (text);
-    //gtk_widget_show (row);
     gtk_list_box_insert (GTK_LIST_BOX (list),row,-1);
+    gtk_widget_show (row);
   }
-  //gtk_widget_show (list);
+  gtk_widget_show (list);
   gtk_container_add (GTK_CONTAINER (box),list);
   g_timer_stop (time_row);
   printf ("Time to add row: %lf\n",g_timer_elapsed (time_row,&msecs));
-  //gtk_widget_show_all (box);
+  //gtk_widget_show (box);
 
   gtk_main ();
 }
@@ -58,26 +51,33 @@ static void
 activate (GtkApplication *app,
           gpointer        user_data)
 {
-  GtkWidget *window;
+  //static GtkWidget *window;
   GtkWidget *box;
   GtkWidget *button;
   GtkWidget *button_box;
+  GtkWidget *scroll;
+  
+  //window = gtk_dialog_new ();
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
+  scroll = gtk_scrolled_window_new (NULL, NULL);
+  gtk_container_set_border_width (GTK_CONTAINER (scroll), 10);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
 
   box=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 300);
-
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  
-
-  gtk_box_pack_start (box, button_box, TRUE, TRUE, 300);
   button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (next_page), &box);
   gtk_container_add (GTK_CONTAINER (button_box), button);
-  gtk_container_add (GTK_CONTAINER (window), box);
-  gtk_widget_show_all (window);
+  gtk_box_pack_start (GTK_BOX (box), button_box, TRUE, TRUE, 300);
+  gtk_container_add (GTK_CONTAINER (scroll), box);
+  //gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroll), box);
+  g_signal_connect (button, "clicked", G_CALLBACK (next_page), box);
+  
+  //gtk_container_add (GTK_CONTAINER (window), scroll);
+   //gtk_box_pack_start (GTK_DIALOG(window), scroll, TRUE, TRUE, 0);
+  gtk_widget_show (scroll);
+  gtk_widget_show (box);
+  //gtk_widget_show (window);
+  
 }
 
 int main (int argc, 
@@ -95,6 +95,3 @@ int main (int argc,
   return status;
 
 }
-
-
-
