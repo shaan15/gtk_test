9d30726497318bacbbddab299f8b8917bb2d546a
shaan15 <shaan15090@iiitd.ac.in>
2017-04-25 09:28:07 +0530

    scrollwindow added, final working code till task 2

--- gtk_listbox_test.c
+++ gtk_listbox_test.c
@@ -11,7 +11,7 @@ create_row (const gchar *text)
   row = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
   label = gtk_label_new (text);
   gtk_container_add (GTK_CONTAINER (row), label);
-  gtk_widget_show_all(row);
+  gtk_widget_show_all (row);
   return row;
 }
 
@@ -26,14 +26,13 @@ next_page (GtkApplication *app,
 
   scroll = gtk_scrolled_window_new (NULL, NULL);
   gtk_container_set_border_width (GTK_CONTAINER (scroll), 10);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
+                                  GTK_POLICY_AUTOMATIC,
+                                  GTK_POLICY_ALWAYS);
 
   list = gtk_list_box_new ();
   gtk_list_box_set_selection_mode (GTK_LIST_BOX (list), GTK_SELECTION_NONE);
-  //gtk_box_pack_start (box, list, TRUE, TRUE, 300);
-
-  //gtk_container_add (GTK_CONTAINER (scroll), list)
-
+  
   gint i;
   g_timer_reset (time_row);
   g_timer_start (time_row);
@@ -44,15 +43,14 @@ next_page (GtkApplication *app,
     gtk_list_box_insert (GTK_LIST_BOX (list),row,-1);
     gtk_widget_show (row);
   }
-  gtk_box_pack_start (GTK_BOX (box), scroll, TRUE, TRUE, 300);
+  gtk_box_pack_start (GTK_BOX (box), scroll, TRUE, TRUE, 0);
   gtk_container_add (GTK_CONTAINER (scroll),list);
   
   gtk_widget_show (scroll);
   gtk_widget_show (list);
   g_timer_stop (time_row);
   printf ("Time to add row: %lf\n",g_timer_elapsed (time_row,&msecs));
-  //gtk_widget_show (box);
-
+  
   gtk_main ();
 }
 
@@ -70,11 +68,11 @@ activate (GtkApplication *app,
   gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
   
 
-  box=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 300);
+  box=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
   button = gtk_button_new_with_label ("Hello World");
   gtk_container_add (GTK_CONTAINER (button_box), button);
-  gtk_box_pack_start (GTK_BOX (box), button_box, TRUE, TRUE, 300);
+  gtk_box_pack_start (GTK_BOX (box), button_box, FALSE, FALSE, 0);
   g_signal_connect (button, "clicked", G_CALLBACK (next_page), box);
   
   gtk_container_add (GTK_CONTAINER (window), box);
