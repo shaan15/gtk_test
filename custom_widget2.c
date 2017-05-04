cc839391295eca7af69ef7fd7b028d5fe46c21be
shaan15 <shaan15090@iiitd.ac.in>
2017-05-04 13:00:32 +0530

    Second attempt at custom widget drawing, incomplete

---
+++ custom_widget2.c
@@ -0,0 +1,144 @@
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <string.h>
+#include <libintl.h>
+#include <locale.h>
+#include <math.h>
+#include <gdk/gdkkeysyms.h>
+
+G_DEFINE_TYPE (MyButton, my_button, GTK_TYPE_BUTTON)
+
+GTimer *time_row;
+unsigned long msecs;
+
+static void
+my_button_add (GtkContainer *button, 
+				GtkWidget *child)
+{
+
+}
+
+static void 
+my_button_class_init (MyButtonClass *Class)
+{
+	GtkContainerClass *container_class = GTK_CONTAINER_CLASS (class);
+
+	container_class->add = my_button_add;
+}
+
+static void
+my_button_finalise (GObject *object)
+{
+	G_OBJECT_CLASS (my_button_parent_class)->finalise (object);
+}
+
+static gboolen
+my_button_draw (GtkWidget *widget, cairo_t *cr)
+{
+	MyButton *button = MY_BUTTON (widget);
+	GtkStyleContext *context;
+	gint width, height;
+	context = gtk_widget_get_style_context (widget);
+	width = gtk_widget_get_allocated_width (widget);
+	height = gtk_widget_get_allocated_height (widget);
+
+	gtk_render_background (context, cr, 0, 0, width, height);
+	gtk_render_frame (context, cr, 0, 0, width, height);
+
+	//Drawing the widget 
+
+	return FALSE;
+}
+
+static GtkWidget * 
+create_row (const gchar *text)
+{
+  GtkWidget *row, *label, *button;
+  row = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
+  label = gtk_label_new (text);
+  gtk_container_add (GTK_CONTAINER (row), label);
+  gtk_widget_show_all (row);
+  return row;
+}
+
+static void 
+next_page (GtkApplication *app, 
+           GtkWidget *box)
+{
+  time_row=g_timer_new();
+  GtkWidget *list,*row, *scroll;
+  gchar *text;
+  gtk_init (NULL, NULL);
+
+  scroll = gtk_scrolled_window_new (NULL, NULL);
+  gtk_container_set_border_width (GTK_CONTAINER (scroll), 10);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
+                                  GTK_POLICY_AUTOMATIC,
+                                  GTK_POLICY_ALWAYS);
+
+  list = gtk_list_box_new ();
+  gtk_list_box_set_selection_mode (GTK_LIST_BOX (list), GTK_SELECTION_NONE);
+  
+  gint i;
+  g_timer_reset (time_row);
+  g_timer_start (time_row);
+  for (i=1;i<=2000;i++)
+  {
+    text=g_strdup_printf("Row %d",i);
+    row=create_row (text);
+    gtk_list_box_insert (GTK_LIST_BOX (list),row,-1);
+    gtk_widget_show (row);
+  }
+  gtk_box_pack_start (GTK_BOX (box), scroll, TRUE, TRUE, 0);
+  gtk_container_add (GTK_CONTAINER (scroll),list);
+  
+  gtk_widget_show (scroll);
+  gtk_widget_show (list);
+  g_timer_stop (time_row);
+  printf ("Time to add row: %lf\n",g_timer_elapsed (time_row,&msecs));
+  
+  gtk_main ();
+}
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+  GtkWidget *box;
+  GtkWidget *button;
+  GtkWidget *button_box;
+  
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
+  
+
+  box=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  button = gtk_button_new_with_label ("Hello World");
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+  gtk_box_pack_start (GTK_BOX (box), button_box, FALSE, FALSE, 0);
+  g_signal_connect (button, "clicked", G_CALLBACK (next_page), box);
+  
+  gtk_container_add (GTK_CONTAINER (window), box);
+  gtk_widget_show (box);
+  gtk_widget_show_all (window);
+  
+}
+
+int main (int argc, 
+          char **argv)
+{
+
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+
+}
